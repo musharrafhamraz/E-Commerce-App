fbed2df422dc534eb9665f4ac8cbd25eb1444978
musharrafhamraz <musharrafhamraz02@gmail.com>
2024-08-06 11:14:20 +0500

    added controller placeholders for onboarding functions.

---
+++ lib/feature/authentication/controllers/onboarding/onboarding_controller.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+
+class OnboardingController extends GetxController {
+  static OnboardingController get instance => Get.find();
+
+  void updatePageIndicator(index) {}
+
+  void dotNavigationClick(index) {}
+
+  void nextPage() {}
+
+  void skipPage() {}
+}
